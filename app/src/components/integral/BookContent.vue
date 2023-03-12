35903d9e85cab8e1d383f932c468b230e815475b
fyh <1913068672@qq.com>
2023-03-12 16:24:33 +0800

    fix identation

--- app/src/components/integral/BookContent.vue
+++ app/src/components/integral/BookContent.vue
@@ -17,56 +17,56 @@
       </div>
     </div>
     <div v-else>
-          <div v-if="content.type == 'definition'">
-            <MathEquation v-bind:data="'\\(' + content.latex_str + '\\)'" class="indented-text"
-              v-on:click.native="openFile(content.path)"
-              style="cursor:pointer"/>
-          </div>
-          <div v-if="content.type == 'problem'">
-            <MathEquation v-bind:data="'\\(' + content.latex_str + '\\)'" class="indented-text"
-              v-on:click.native="openFile(content.path)"
-              style="cursor:pointer"/>
-            <span v-if="'latex_conds' in content && content.latex_conds.length > 0">
-              <span class="math-text indented-text">for &nbsp;</span>
-              <span v-for="(cond, index) in content.latex_conds" :key="index">
-                <span v-if="index > 0">, &nbsp;</span>
-                <MathEquation v-bind:data="'\\(' + cond + '\\)'"/>
-              </span>
-            </span>
-          </div>
-          <div v-if="content.type == 'axiom'">
-            <MathEquation v-bind:data="'\\(' + content.latex_str + '\\)'" class="indented-text"/>
-            <span v-if="'latex_conds' in content && content.latex_conds.length > 0">
-              <span class="math-text indented-text">for &nbsp;</span>
-              <span v-for="(cond, index) in content.latex_conds" :key="index">
-                <span v-if="index > 0">, &nbsp;</span>
-                <MathEquation v-bind:data="'\\(' + cond + '\\)'"/>
-              </span>
-            </span>
-          </div>
-          <div v-if="content.type == 'table'" style="margin: 5px">
-            <table style="border-collapse: collapse">
-              <tr>
-                <td style="border-style: solid; padding: 3px">
-                  <MathEquation v-bind:data="'\\(' + '{x}' + '\\)'"/>
-                </td>
-                <td v-for="(entry, index) in content.latex_table" :key="index"
-                    style="border-style: solid; padding: 3px">
-                  <MathEquation v-bind:data="'\\(' + entry.x + '\\)'"/>
-                </td>
-              </tr>
-              <tr>
-                <td style="border-style: solid; padding: 3px">
-                  <MathEquation v-bind:data="'\\(' + content.funcexpr + '\\)'"/>
-                </td>
-                <td v-for="(entry, index) in content.latex_table" :key="index"
-                    style="border-style: solid; padding: 3px">
-                  <MathEquation v-bind:data="'\\(' + entry.y + '\\)'"/>
-                </td>
-              </tr>
-            </table>
-          </div>
-        </div>
+      <div v-if="content.type == 'definition'">
+        <MathEquation v-bind:data="'\\(' + content.latex_str + '\\)'" class="indented-text"
+          v-on:click.native="openFile(content.path)"
+          style="cursor:pointer"/>
+      </div>
+      <div v-if="content.type == 'problem'">
+        <MathEquation v-bind:data="'\\(' + content.latex_str + '\\)'" class="indented-text"
+          v-on:click.native="openFile(content.path)"
+          style="cursor:pointer"/>
+        <span v-if="'latex_conds' in content && content.latex_conds.length > 0">
+          <span class="math-text indented-text">for &nbsp;</span>
+          <span v-for="(cond, index) in content.latex_conds" :key="index">
+            <span v-if="index > 0">, &nbsp;</span>
+            <MathEquation v-bind:data="'\\(' + cond + '\\)'"/>
+          </span>
+        </span>
+      </div>
+      <div v-if="content.type == 'axiom'">
+        <MathEquation v-bind:data="'\\(' + content.latex_str + '\\)'" class="indented-text"/>
+        <span v-if="'latex_conds' in content && content.latex_conds.length > 0">
+          <span class="math-text indented-text">for &nbsp;</span>
+          <span v-for="(cond, index) in content.latex_conds" :key="index">
+            <span v-if="index > 0">, &nbsp;</span>
+            <MathEquation v-bind:data="'\\(' + cond + '\\)'"/>
+          </span>
+        </span>
+      </div>
+      <div v-if="content.type == 'table'" style="margin: 5px">
+        <table style="border-collapse: collapse">
+          <tr>
+            <td style="border-style: solid; padding: 3px">
+              <MathEquation v-bind:data="'\\(' + '{x}' + '\\)'"/>
+            </td>
+            <td v-for="(entry, index) in content.latex_table" :key="index"
+                style="border-style: solid; padding: 3px">
+              <MathEquation v-bind:data="'\\(' + entry.x + '\\)'"/>
+            </td>
+          </tr>
+          <tr>
+            <td style="border-style: solid; padding: 3px">
+              <MathEquation v-bind:data="'\\(' + content.funcexpr + '\\)'"/>
+            </td>
+            <td v-for="(entry, index) in content.latex_table" :key="index"
+                style="border-style: solid; padding: 3px">
+              <MathEquation v-bind:data="'\\(' + entry.y + '\\)'"/>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
   

5c0c2b5e4747d1048d0c7a3c4ae44936925c8e72
fyh <1913068672@qq.com>
2023-08-03 20:34:45 +0800

    add files

---
+++ app/src/components/book_items/HeaderAdd.vue
@@ -0,0 +1,26 @@
+<template>
+  <form>
+    <div>
+      <label>name:</label><input v-model="header_name" />
+    </div>
+    <div>
+      <label>level:</label><input v-model="header_level" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'HeaderEdit',
+  props: [
+    "header"
+  ],
+
+  data: function () {
+    return {
+      name: this.header.name,
+      level: this.header.level
+    }
+  }
+}
+</script>
---
+++ app/src/components/book_items/HeaderEdit.vue
@@ -0,0 +1,28 @@
+<template>
+  <form>
+    <div>
+      <label>name</label>
+      <input v-model="name"/>
+    </div>
+    <div>
+      <label>level</label>
+      <input v-model="level"/>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'HeaderEdit',
+  props: [
+    "header"
+  ],
+
+  data: function () {
+    return {
+      name: this.header.name,
+      level: this.header.level
+    }
+  }
+}
+</script>

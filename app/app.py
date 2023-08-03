fa96f2bda1dfe5f955972bc528fa0efc58ab17af
fyh <1913068672@qq.com>
2023-08-03 20:00:46 +0800

    fix JSONEncoder deprecated

--- app/app.py
+++ app/app.py
@@ -1,20 +1,17 @@
 """Setup flask app."""
-
-from flask import Flask
 from flask_cors import CORS
-from flask.json import JSONEncoder as _JSONEncoder
-from flask import Flask as _Flask
 
-class JSONEncoder(_JSONEncoder):
+from datetime import date, datetime
+from flask.json.provider import DefaultJSONProvider
+from flask import Flask
+
+class UpdatedJSONProvider(DefaultJSONProvider):
     def default(self, o):
-        if hasattr(o, 'keys') and hasattr(o, '__getitem__'):
-            return dict(o)
-        else:
-            return super().default(o)
-
-class Flask(_Flask):
-    json_encoder = JSONEncoder
+        if isinstance(o, date) or isinstance(o, datetime):
+            return o.isoformat(' ')
+        return super().default(o)
 
 app = Flask(__name__, static_url_path='/static')
+app.json = UpdatedJSONProvider(app)
 CORS(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'

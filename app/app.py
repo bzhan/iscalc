30955fa0d236d94c93ff63810595487cea684064
fyh <1913068672@qq.com>
2023-08-03 20:08:46 +0800

    fix

--- app/app.py
+++ app/app.py
@@ -1,17 +1,8 @@
 """Setup flask app."""
 from flask_cors import CORS
-
-from datetime import date, datetime
-from flask.json.provider import DefaultJSONProvider
 from flask import Flask
 
-class UpdatedJSONProvider(DefaultJSONProvider):
-    def default(self, o):
-        if isinstance(o, date) or isinstance(o, datetime):
-            return o.isoformat(' ')
-        return super().default(o)
 
 app = Flask(__name__, static_url_path='/static')
-app.json = UpdatedJSONProvider(app)
 CORS(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'

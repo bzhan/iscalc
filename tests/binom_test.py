65a2ee7c96f97f693e3b337da10ae808ffa13140
fyh <1913068672@qq.com>
2023-09-14 22:42:09 +0800

    add proof of binomial theorem

--- tests/binom_test.py
+++ tests/binom_test.py
@@ -16,8 +16,16 @@ class BinomTest(unittest.TestCase):
         # Test parsing of json file
         json_file = file.export()
         for i, item in enumerate(json_file['content']):
-            self.assertEqual(compstate.parse_item(file.content[i].parent, item).export(), file.content[i].export())
-
+            aa, bb = compstate.parse_item(file.content[i].parent, item), file.content[i]
+            a, b = aa.export(), bb.export()
+            # if a != b:
+            #     if isinstance(aa, compstate.Goal) and isinstance(bb, compstate.Goal):
+            #         aa.is_finished()
+            #     with open('examples/a.json', 'w', encoding='utf-8') as f:
+            #         json.dump(a, f, indent=4, ensure_ascii=False, sort_keys=True)
+            #     with open('examples/b.json', 'w', encoding='utf-8') as f:
+            #         json.dump(b, f, indent=4, ensure_ascii=False, sort_keys=True)
+            self.assertEqual(a, b)
         # Output to file
         with open('examples/' + file.name + '.json', 'w', encoding='utf-8') as f:
         # with open('../examples/' + file.name + '.json', 'w', encoding='utf-8') as f:
@@ -41,6 +49,7 @@ class BinomTest(unittest.TestCase):
         calc.perform_rule(rules.ExpandDefinition("C"))
         calc = proof.rhs_calc
         calc.perform_rule(rules.ExpandDefinition("C"))
+        assert goal01.is_finished()
         goal02 = file.add_goal("C(n,m)*C(m,r) = C(n,r)*C(n-r, m-r)")
         proof = goal02.proof_by_calculation()
         calc = proof.lhs_calc
@@ -49,6 +58,7 @@ class BinomTest(unittest.TestCase):
         calc = proof.rhs_calc
         calc.perform_rule(rules.OnSubterm(rules.ExpandDefinition("C")))
         calc.perform_rule(rules.FullSimplify())
+        assert goal02.is_finished()
         goal03 = file.add_goal("C(n,m-1)+C(n,m) = C(n+1, m)")
         proof = goal03.proof_by_calculation()
         calc = proof.lhs_calc
@@ -74,6 +84,7 @@ class BinomTest(unittest.TestCase):
         calc.perform_rule(rules.OnLocation(rules.ApplyIdentity(s1, s2), "0"))
         calc = proof.rhs_calc
         calc.perform_rule(rules.OnSubterm(rules.ExpandDefinition("C")))
+        assert goal03.is_finished()
         goal04 = file.add_goal("C(m+n+1,m) = SUM(i, 0, m, C(n+i, i))", conds=["m>=0", "n>=0"], fixes=fixes)
         proof = goal04.proof_by_induction("m")
         base_proof = proof.base_case.proof_by_calculation()
@@ -95,7 +106,69 @@ class BinomTest(unittest.TestCase):
         calc.perform_rule(rules.Equation(s1, s2))
         calc.perform_rule(rules.ApplyEquation(goal03.goal))
         calc.perform_rule(rules.FullSimplify())
-        print(goal04)
+        assert goal04.is_finished()
+        fixes = dict()
+        fixes['n'] = parser.parse_expr('$int')
+        fixes['k'] = parser.parse_expr('$int')
+        goal05 = file.add_goal("(x+y)^n = SUM(k,0,n,C(n, k)*x^k*y^(n-k))", conds=['x!=0', 'y!=0', 'n>0'], fixes=fixes)
+        proof = goal05.proof_by_induction('n', 1)
+        base_proof = proof.base_case.proof_by_calculation()
+        calc = base_proof.rhs_calc
+        cond = parser.parse_expr('k=0', fixes=fixes)
+        calc.perform_rule(rules.SplitSummation(cond))
+        calc.perform_rule(rules.FullSimplify())
+        calc.perform_rule(rules.OnSubterm(rules.ExpandDefinition("C")))
+        calc.perform_rule(rules.FullSimplify())
+        induct_proof = proof.induct_case.proof_by_calculation()
+        calc = induct_proof.lhs_calc
+        s1 = parser.parse_expr("(x+y)^(n+1)", fixes=fixes)
+        s2 = parser.parse_expr("(x+y)*(x+y)^n", fixes=fixes)
+        calc.perform_rule(rules.Equation(s1, s2))
+        calc.perform_rule(rules.OnLocation(rules.ApplyInductHyp(), '1'))
+        calc.perform_rule(rules.ExpandPolynomial())
+
+        s1 = parser.parse_expr("x * SUM(k, 0, n, x ^ k * y ^ (-k + n) * C(n,k))", fixes=fixes)
+        s2 = parser.parse_expr("SUM(k, 0, n, x * (x ^ k * y ^ (-k + n) * C(n,k)))", fixes=fixes)
+        calc.perform_rule(rules.Equation(s1, s2))
+        s1 = parser.parse_expr("y * SUM(k, 0, n, x ^ k * y ^ (-k + n) * C(n,k))", fixes=fixes)
+        s2 = parser.parse_expr("SUM(k, 0, n, y * (x ^ k * y ^ (-k + n) * C(n,k)))", fixes=fixes)
+        calc.perform_rule(rules.Equation(s1, s2))
+        s1 = parser.parse_expr("x * (x ^ k * y ^ (-k + n) * C(n,k))", fixes=calc.ctx.get_fixes())
+        s2 = parser.parse_expr("C(n,k)*x^(k+1)*y^(n-k)", fixes=calc.ctx.get_fixes())
+        calc.perform_rule(rules.Equation(s1, s2))
+        s1 = parser.parse_expr("y * (x ^ k * y ^ (-k + n) * C(n,k))", fixes=calc.ctx.get_fixes())
+        s2 = parser.parse_expr("C(n,k)*x^k*y^(n-k+1)", fixes=calc.ctx.get_fixes())
+        calc.perform_rule(rules.Equation(s1, s2))
+        cond = parser.parse_expr("k=n", fixes=fixes)
+        calc.perform_rule(rules.OnLocation(rules.SplitSummation(cond), '0'))
+        cond = parser.parse_expr("k=0", fixes=fixes)
+        calc.perform_rule(rules.OnLocation(rules.SplitSummation(cond), '1'))
+        calc.perform_rule(rules.FullSimplify())
+        calc.perform_rule(rules.OnLocation(rules.ExpandDefinition('C'), '0.1.1'))
+        calc.perform_rule(rules.OnLocation(rules.ExpandDefinition('C'), '1.1'))
+        calc.perform_rule(rules.OnLocation(rules.ChangeSummationIndex("1"), '0.0.0'))
+        calc.perform_rule(rules.FullSimplify())
+        s1 = parser.parse_expr("SUM(i, 1, n, x ^ i * y ^ (-i + n + 1) * C(n,i - 1)) + SUM(i, 1, n, x ^ i * y ^ (-i + n + 1) * C(n,i))", fixes=calc.ctx.get_fixes())
+        s2 = parser.parse_expr("SUM(i, 1, n, x ^ i * y ^ (-i + n + 1) * C(n,i - 1) + x ^ i * y ^ (-i + n + 1) * C(n,i))", fixes=calc.ctx.get_fixes())
+        calc.perform_rule(rules.Equation(s1, s2))
+        s1 = parser.parse_expr(
+            "x ^ i * y ^ (-i + n + 1) * C(n,i - 1) + x ^ i * y ^ (-i + n + 1) * C(n,i)",
+            fixes=calc.ctx.get_fixes())
+        s2 = parser.parse_expr(
+            "x ^ i * y ^ (-i + n + 1) * (C(n,i - 1) + C(n,i))",
+            fixes=calc.ctx.get_fixes())
+        calc.perform_rule(rules.Equation(s1, s2))
+        calc.perform_rule(rules.OnLocation(rules.ApplyEquation(goal03.goal) ,'0.0.0.1'))
+        calc = induct_proof.rhs_calc
+        cond = parser.parse_expr("k=0", fixes=calc.ctx.get_fixes())
+        calc.perform_rule(rules.SplitSummation(cond))
+
+        cond = parser.parse_expr("i=n+1", fixes=calc.ctx.get_fixes())
+        calc.perform_rule(rules.OnLocation(rules.SplitSummation(cond), '1'))
+        calc.perform_rule(rules.OnLocation(rules.ExpandDefinition('C'), '0.0.1'))
+        calc.perform_rule(rules.OnLocation(rules.ExpandDefinition('C'), '1.1.0.1'))
+        calc.perform_rule(rules.FullSimplify())
+        self.checkAndOutput(file)
 
 if __name__ == "__main__":
     unittest.main()

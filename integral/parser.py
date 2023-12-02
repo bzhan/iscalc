6b69c5884b871f69db229dc0bdfcecaca0b2bfcf
fyh <1913068672@qq.com>
2023-12-02 10:34:05 +0800

    remove some code

--- integral/parser.py
+++ integral/parser.py
@@ -43,7 +43,6 @@ grammar = r"""
 
     ?times: times "*" pow -> times_expr        // priority 70
         | times "/" pow -> divides_expr 
-        | times "//" pow -> divides_expr2 
         | times "%" pow -> modulo_expr
         | pow
 
@@ -104,9 +103,6 @@ class ExprTransformer(Transformer):
             return expr.Const(Fraction(a.val) / Fraction(b.val))
         else:
             return expr.Op("/", a, b)
-
-    def divides_expr2(self, a, b):
-        return expr.Op("/", a, b)
 
     def modulo_expr(self, a, b):
         if a.ty == expr.CONST and b.ty == expr.CONST and isinstance(a.val, int) and isinstance(b.val, int):
